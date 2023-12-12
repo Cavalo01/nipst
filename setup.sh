7abb86947a6e8e391fb4084478ab9322878e3220
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-12-12 01:10:58 -0300

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+echo '
+#!/bin/bash
+
+target_url=$1
+robots_txt_url="$target_url/robots.txt"
+disallowed_pages=()
+
+response_code=$(curl -sL -w "%{http_code}" "$robots_txt_url" -o /dev/null)
+
+if [[ $response_code -eq 200 ]]; then
+  disallowed_pages=($(curl -sL "$robots_txt_url" | grep -oP "(?<=Disallow: ).*"))
+
+  if [[ ${#disallowed_pages[@]} -eq 0 ]]; then
+   echo "Cant find No-Indexed Pages."
+  else
+    echo "Not-Indexed Pages:"
+    for page in "${disallowed_pages[@]}"; do
+      echo "$page"
+    done
+  fi
+else
+  echo "robots.txt Not Found."
+fi
+' > nipst
+chmod +x nipst
+mv ./nipst ../nipst
+cd ~
+echo "Use with: ./nipst website.com"
